b8384ffa9973b576b70b6a3d6d220c8371c3e231
maksim.skavysh <maksim.skavysh@gmail.com>
2017-09-19 18:16:24 +0300

    fix

--- ML-2017/ml_homework_1.py
+++ ML-2017/ml_homework_1.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 
 
 class Rect:
-    def __init__(self, x1=0, y1=0, x2=0, y2=0):
+    def __init__(self, x1=0.0, y1=0.0, x2=0.0, y2=0.0):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.square = 0
 
     def check_dot(self, dot):
         (x, y) = dot
@@ -26,36 +27,48 @@ class Rect:
             self.y2 = y
 
     def plot(self, color='b'):
-        plt.plot((self.x1, self.x2, self.x2, self.x1, self.x1), (self.y1, self.y1, self.y2, self.y2, self.y1), color=color)
+        plt.plot((self.x1, self.x2, self.x2, self.x1, self.x1), (self.y1, self.y1, self.y2, self.y2, self.y1),
+                 color=color)
+
+    def calculate_square(self):
+        self.square = (self.x2 - self.x1) * (self.y2 - self.y1)
+        return self.square
 
 
 def create_marks(S, square: Rect):
     marks = []
-    print(len(S))
     for i in range(0, len(S), 1):
         mark = 1 if square.check_dot(S[i]) else 0
         marks.append(mark)
     return marks
 
-m = 10
+
+def calculate_true_risk(hypotesa: Rect, true_rect: Rect):
+    return 1 - hypotesa.square / true_rect.square
+
+m = 1000
 main_rect = Rect(0, 0, 1, 1)
+main_rect.calculate_square()
+
 q_side = 0.5 ** 0.5
 q_rect = Rect(0, 0, q_side, q_side)
+q_rect.square = 0.5
 
 S = np.random.uniform(0, 1, (m, 2))
 Y = create_marks(S, q_rect)
-print(Y)
-
 
 h_rect = Rect(1, 1, 0, 0)
 for i in range(0, len(S), 1):
     if Y[i] == 1:
         h_rect.update_bounds(S[i])
+h_rect.calculate_square()
 
-main_rect.plot('black')
-q_rect.plot('black')
-h_rect.plot('r')
-for i in range(0, len(S), 1):
-    color = 'g' if Y[i] == 1 else 'r'
-    plt.scatter(S[i, 0], S[i, 1], s=5, facecolors=color, edgecolors=color)
-plt.show()
+print(calculate_true_risk(h_rect, q_rect))
+
+# main_rect.plot('black')
+# q_rect.plot('black')
+# h_rect.plot('r')
+# for i in range(0, len(S), 1):
+#     color = 'g' if Y[i] == 1 else 'r'
+#     plt.scatter(S[i, 0], S[i, 1], s=5, facecolors=color, edgecolors=color)
+# plt.show()
